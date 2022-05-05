be432faa5b3c94c2a061dbe018eb2b4119fc8080
Yan Romano <yanromano1@gmail.com>
2022-05-05 19:28:09 -0300

    Começando py principal e normalizando a base

---
+++ InsperPaises.py
@@ -0,0 +1,21 @@
+from haversine import haversine
+from esta_na_lista import esta_na_lista
+from adiciona_em_ordem import adiciona_em_ordem
+from sorteia_letra import sorteia_letra
+from sorteia_pais import sorteia_pais
+from basenormalizada import *
+from random import choice
+
+print(" ============================")
+print("|                            |")
+print("| Bem-vindo ao Insper Países |")
+print("|                            |")
+print(" ==== Design de Software ====")
+print()
+print(" Comandos:")
+print()
+print("     dica       - entra no mercado de dicas")
+print("     desisto    - desiste da rodada")
+print("     inventario - exibe sua posição")
+print()
+print("Um país foi escolhido, tente adivinhar!")
